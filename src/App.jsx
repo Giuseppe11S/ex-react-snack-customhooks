c38d969518b4f2c8c562de3bb937fc8478b02c05
Giuseppe11S <senagiuseppe11@gmail.com>
2026-02-02 17:56:09 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import useSwitch from './useSwitch'
+import useDate from './useDate';
 import './App.css'
 
 function App() {
   const {isOn, toggle} = useSwitch();
+  const {time} = useDate();
 
   return (
     <div>
       <button onClick={toggle}>Cambia Stato</button>
       <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+      <h1>Data e ora attuali:</h1>
+      <p>{time.toLocaleString()}</p>
     </div>
   );
 }
